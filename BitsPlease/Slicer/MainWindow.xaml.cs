f46f23c50ea5e935b7e3b59abe09f9e6c7051631
GeorgeKru <33947033+GeorgeKru@users.noreply.github.com>
2017-11-25 15:47:21 -0800

    Add files via upload

--- BitsPlease/Slicer/MainWindow.xaml.cs
+++ BitsPlease/Slicer/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace Slicer
 {
@@ -24,5 +25,22 @@ namespace Slicer
         {
             InitializeComponent();
         }
+
+
+        private void Trim(object sender, RoutedEventArgs e)
+        {
+            String i, ss, t; //i = path ss= start point t = duration
+
+            if (!String.IsNullOrEmpty(start.Text) && (!String.IsNullOrEmpty(end.Text)))
+            {
+                i = "C:\\Users\\goshe\\Desktop\\IMG_0163.mp4";
+                ss = start.Text;
+                t = end.Text;
+
+                Process.Start("ffmpeg.exe", "-i IMG_0163.mp4"+"-"+ ss +"-"+ t +"OutputVideoFile.mp4");
+            }
+        }
+
+       
     }
 }
