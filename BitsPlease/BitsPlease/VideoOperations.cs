40c4fa8f281386760218ec74dd3e01a3d25950d2
Alex Mason <alex@accelgames.com>
2017-11-25 15:25:21 -0800

    working on video operations

---
+++ BitsPlease/BitsPlease/VideoOperations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FFmpeg.AutoGen;
+
+namespace BitsPlease
+{
+    class VideoOperations
+    {
+
+      public static void Slice(
+        string inputFile,
+        string outputFile,
+        uint x,
+        uint y,
+        uint width,
+        uint height)
+    {
+      
+    }
+
+
+    }
+}
