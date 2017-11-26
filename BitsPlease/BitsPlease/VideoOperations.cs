28b78b383141dd9feeb3cacbf17931c03cb0bf89
Alex Mason <alex@accelgames.com>
2017-11-25 16:37:04 -0800

    video operations

--- BitsPlease/BitsPlease/VideoOperations.cs
+++ BitsPlease/BitsPlease/VideoOperations.cs
@@ -3,24 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using FFmpeg.AutoGen;
+using System.IO;
+using System.Diagnostics;
 
 namespace BitsPlease
 {
-    class VideoOperations
-    {
+  class VideoOperations
+  {
+    private static string FFMPEG = "ffmpeg.exe";
+    private static string BASEARGS = "-y ";
+    private static string FFPROBE = "ffprobe.exe";
+    
 
-      public static void Slice(
-        string inputFile,
-        string outputFile,
-        uint x,
-        uint y,
-        uint width,
-        uint height)
+    public static void PerformCrop(
+      string inputFile,
+      string outputFile,
+      uint x,
+      uint y,
+      uint width,
+      uint height)
     {
       
     }
 
 
+    Process FfmpegProcess()
+    {
+      Process process = new Process();
+      process.StartInfo.FileName = FFMPEG;
+      process.StartInfo.Arguments = BASEARGS;
+      process.StartInfo.CreateNoWindow = true;
+      process.StartInfo.RedirectStandardError = true;
+      process.StartInfo.RedirectStandardOutput = true;
+      process.StartInfo.UseShellExecute = false;
+
+
+      return process;
     }
+
+  }
 }
