9055de5cd724e8378e642a4b1cb4d0b2026f9c07
Hammerlord <nepharus@gmail.com>
2017-12-02 15:43:18 -0800

    PlaybackTools as a UserControl.

---
+++ BitsPlease/BitsPlease/PlaybackTools.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BitsPlease
+{
+    /// <summary>
+    /// Interaction logic for PlaybackTools.xaml
+    /// </summary>
+    public partial class PlaybackTools : UserControl
+    {
+        public PlaybackTools()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPlayClicked(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void OnPauseClicked(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
